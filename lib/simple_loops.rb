30bf124ade281ae6187dd0101796e6e0b9756c9b
Chana Kupetz <chanakupetz@gmail.com>
2020-07-23 02:54:42 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,23 @@
-# Write your methods here
+def loop_message_five_times(message)
+counter = 0
+while counter <= 5
+  puts message
+  counter += 1
+end
+end
+
+def loop_message_n_times(message, n)
+counter = 0
+while counter <= n
+  puts message
+  counter +=1
+end
+end
+
+def output_array(array)
+  puts array
+end
+
+def return_string_array(array)
+  return array
+end
